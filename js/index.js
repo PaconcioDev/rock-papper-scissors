49a6fff0e1bc8c69e51a9e41f0bbc39e8f56cf65
Francisco Gomez <pgomezdev@gmail.com>
2022-10-11 09:44:45 -0500

    V_0.1.3 Displays added when input checked but i want to do it completely  with js

--- js/index.js
+++ js/index.js
@@ -1,50 +1,27 @@
-import { paco, paolo, sre } from "./characters.js";
-let characters = [paco, paolo, sre];
+function showDescription() {
+  const pacoCard = document.getElementById("paco");
+  const paoloCard = document.getElementById("paolo");
+  const sreCard = document.getElementById("sre");
 
-let pacoCard = document.getElementById("paco");
-let paoloCard = document.getElementById("paolo");
-let sreCard = document.getElementById("sre");
+  const pacoDisplay = document.getElementById("paco-display");
+  const paoloDisplay = document.getElementById("paolo-display");
+  const sreDisplay = document.getElementById("sre-display");
 
-let charactersDisplay = document.getElementById("characters-display");
+  if (pacoCard.checked) {
+    pacoDisplay.style.display = "flex";
+  } else {
+    pacoDisplay.style.display = "none";
+  }
+  if (paoloCard.checked) {
+    paoloDisplay.style.display = "flex";
+  } else {
+    paoloDisplay.style.display = "none";
+  }
+  if (sreCard.checked) {
+    sreDisplay.style.display = "flex";
+  } else {
+    sreDisplay.style.display = "none";
+  }
+}
 
-characters.forEach(function (character) {
-  character.addEventListener("change", () => {
-    charactersDisplay.innerHTML = `<section id="character-info">
-        <div class="character-description">
-         <h3>${character.name}</h3>
-         <p>
-          ${character.info}
-        </p>
-        <h4>Attacks</h4>
-        <p>
-        ${character.attacksInfo}
-        </p>
-        </div>
-    <div class="attacks-container">
-        <button><img src="imgs/Cobblestone.webp" alt="Rock" />Rock</button>
-        <button><img src="imgs/Papel.webp" alt="Papper" />Papper</button>
-        <button><img src="imgs/Shear.png" alt="Scissors" />Scissors</button>
-    </div>
-    </section>
-    <button id="start-btn">START</button>`;
-  });
-});
-
-/* `<section id="character-info">
-        <div class="character-description">
-            <h3>${character.name}</h3>
-            <p>
-            ${character.info}
-            </p>
-            <h4>Attacks</h4>
-            <p>
-            ${character.attacksInfo}
-            </p>
-        </div>
-        <div class="attacks-container">
-            <button><img src="imgs/Cobblestone.webp" alt="Rock" />Rock</button>
-            <button><img src="imgs/Papel.webp" alt="Papper" />Papper</button>
-            <button><img src="imgs/Shear.png" alt="Scissors" />Scissors</button>
-        </div>
-        </section>
-        <button id="start-btn">START</button>` */
+window.addEventListener('load', showDescription) 
