94d66ebfdd2090df08b9ed7b0af86dd13f850b12
Francisco Gomez <pgomezdev@gmail.com>
2023-01-10 20:54:46 -0500

    Updates

--- assets/characters.js
+++ assets/characters.js
@@ -10,21 +10,21 @@ export class Character {
 
 export let paco = new Character(
   "Paco",
-  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officia dolores neque reprehenderit voluptas voluptates nemo! Eligendi voluptatibus expedita earum recusandae quidem, optio amet harum impedit, vero praesentium ipsam. Recusandae.",
-  "This character was nerfed by the ancient gods, nowdays he can only use 2 attacks, choose wisely",
+  'He was part of a dark cult called "The knights of the blood", this cult belive in a God named Zukkhu. Since Paco was born everyone told him that he was the most powerful blood knight and that he was destinated to big things, but when Paco was growing up Zukkhu himself knew that Paco could kill him one day, so Zukkhu cursed Paco.',
+  "This character was cursed by the Zukkhu the ancient god, nowdays he can only use 2 attacks, choose wisely",
   "https://64.media.tumblr.com/e438a7fcb187daddc11a4654303bb056/tumblr_pi59lbdFVm1w4trzf_400.gif"
 );
 
 export let paolo = new Character(
   "Paolo",
-  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officia dolores neque reprehenderit voluptas voluptates nemo! Eligendi voluptatibus expedita earum recusandae quidem, optio amet harum impedit, vero praesentium ipsam. Recusandae.",
-  "This character was nerfed by the ancient gods, nowdays he can only use 1 attack, choose wisely",
+  "One day Paolo was just bored, so he stealed an armor from a strager, but he didn't know that the armor was made to contain the powers of certain blood knight and now Paolo doesn't know how to take the armor off",
+  "This character was nerfed by 'The Zukkhu armor', he can only use 1 attack, choose wisely",
   "https://i.pinimg.com/originals/d0/23/f8/d023f8ac6daa52386d49b54cd1feba81.gif"
 );
 
 export let sre = new Character(
   "Sr. E",
-  "It's something that our mind just doesn't understand, in it's world they used to call it 'Una mona china'",
+  "It's something that our mind just doesn't understand, in it's world they used to call it 'Una mona china', but in this world everyone calls it 'Zukkhu'",
   "This character has the power to use all the attacks",
   "http://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/229aea930cdd921.png"
 );
